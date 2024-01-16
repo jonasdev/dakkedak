0b447be5f02ae751b787dab8d1462c327439015e
Benjamin Vølund <au578358@uni.au.dk>
2024-01-16 20:34:53 +0100

    Added logs

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -35,6 +35,8 @@ export const getStaticPaths = async () => {
     params: { category: category.slug.toString() },
   }));
 
+  console.log("Made it past getStaticPaths");
+
   return {
     paths,
     fallback: false,
@@ -47,17 +49,22 @@ interface Params {
 
 export const getStaticProps = async ({ params }: { params: Params }) => {
   const { category } = params || {};
+  console.log("category: ", category);
   const currentCategory = categories.find((cat) => cat.slug === category);
-
+  console.log("currentCategory: ", currentCategory);
   if (!currentCategory) {
     return {
       notFound: true,
     };
   }
 
+  console.log("Made it past currentCategory");
+
   const products = await getFeeds({
     category: currentCategory.slug,
   });
+
+  console.log("Made it past products: ", products?.length);
 
   return {
     props: {
